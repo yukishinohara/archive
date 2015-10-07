4d8058c00863b16456107e23384738c4e878571a
Yuki Shinohara <ys6215@voxel27.doc.ic.ac.uk>
2015-10-07 18:58:43 +0100

    Test 04.

--- push.sh
+++ push.sh
@@ -7,8 +7,20 @@ cd ../archive
 rm -rf labs.zip.tmp
 
 if [ $RET = 0 ]; then
-  git add -A . && git commit && git push -u origin master && echo 'done.'
+  echo 'zipped.'
 else
   echo 'aborted.'
+  exit 1
 fi
+
+git add -A . && git commit
+RET=$?
+if [ $RET = 0 ]; then
+  echo 'git committed.'
+else
+  echo 'aborted.'
+  exit 1
+fi
+
+git push -u origin master && echo 'done.'
 
