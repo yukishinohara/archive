f38503d8b2b185c6d75a8e181b86a3104767260b
Yuki Shinohara <ys6215@voxel27.doc.ic.ac.uk>
2015-10-07 18:25:55 +0100

    Test 01.

--- push.sh
+++ push.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-mv labs.zip labs.zip.tmp && zip -e -r labs.zip ../labs && rm -rf labs.zip.tmp
-echo 'done'
+mv labs.zip labs.zip.tmp && zip -e -r labs.zip ../labs && rm -rf labs.zip.tmp && git add -A . && git commit && git push -u origin master && echo 'done.'
 
