e225a3e44e24153f76824e1653c017c05637ab76
Yuki Shinohara <ys6215@voxel27.doc.ic.ac.uk>
2015-10-07 19:12:54 +0100

    Test 06.

--- fetchupdate.sh
+++ fetchupdate.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 read -p "Are you sure? " -n 1 -r
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-    # do dangerous stuff
-    git pull
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+  echo ' '
+  git pull
+else
+  echo 'canceled.'
+  exit 1
 fi
 
+cd ../labs
+unzip -o ../archive/labs.zip
+RET=$?
+cd -
+
+if [ $RET = 0 ]; then
+  echo 'done.'
+else
+  echo 'aborted.'
+  exit 1
+fi
